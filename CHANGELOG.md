78cb7851460fff987d5a94b82ea2016d0d38ceff
David house <davidahouse@gmail.com>
2019-04-06 15:49:59 -0400

    :construction: updates to readme and license

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,5 @@
+## git-lost
+
 ## 0.1.0
 
 - initial release :tada:
--- bin/git-lost.js
+++ bin/git-lost.js
@@ -3,24 +3,28 @@ const fs = require('fs')
 const chalk = require('chalk')
 const clear = require('clear')
 const figlet = require('figlet')
-const path = require('path')
 const conf = require('rc')('git-lost', {
   // defaults
   workingFolder: '.',
   showDirty: true,
-  showClean: false
+  showClean: false,
 })
 
 clear()
 console.log(chalk.yellow(figlet.textSync('git-lost', {horizontalLayout: 'full'})))
+console.log(chalk.yellow('version 0.1.0'))
+console.log(chalk.yellow('searching for repositories in ' + conf.workingFolder))
 
 findRepos(conf.workingFolder)
 
+/**
+ * findRepos
+ * @param {string} folder The folder to find repositories in
+ */
 function findRepos(folder) {
-
   if (fs.existsSync(folder + '/.git')) {
     const simpleGit = require('simple-git')(folder)
-    simpleGit.status(function(err,result) {
+    simpleGit.status(function(err, result) {
       const basename = folder.replace(conf.workingFolder, '')
       if (result.files.length > 0) {
         if (conf.showDirty) {
@@ -33,8 +37,7 @@ function findRepos(folder) {
       }
     })
   } else {
-    let files = fs.readdirSync(folder)
-    const folders = fs.readdirSync(folder).filter(function(file) {
+    fs.readdirSync(folder).filter(function(file) {
       if (fs.statSync(folder+'/'+file).isDirectory()) {
         findRepos(folder + '/' + file)
       }
